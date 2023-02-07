ede8b56caed2c82e756277f7d9556b236d3c5cff
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-02-08 08:24:37 +0900

    Feat : Preview 추가

---
+++ SoleApp/SoleApp/Extension/UIViewControllerRepresentable.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewControllerRepresentable.swift
+//  SoleApp
+//
+//  Created by SUN on 2023/02/08.
+//
+
+import SwiftUI
+
+struct ViewControllerPreview: UIViewControllerRepresentable {
+    let viewControllerBuilder: () -> UIViewController
+
+    init(_ viewControllerBuilder: @escaping () -> UIViewController) {
+        self.viewControllerBuilder = viewControllerBuilder
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return viewControllerBuilder()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
+    
+    
+}
---
+++ SoleApp/SoleApp/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  SoleApp
+//
+//  Created by SUN on 2023/02/06.
+//
+
+import UIKit
+import SnapKit
+import SwiftUI
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+}
+
+struct LoginViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerPreview {
+            LoginViewController()
+        }
+    }
+}
