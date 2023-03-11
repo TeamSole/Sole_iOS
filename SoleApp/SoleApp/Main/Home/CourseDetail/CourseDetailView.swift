b263a6892effa036d4634bb454288b323d41652a
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-03-11 16:28:35 +0900

    Feat: profile section View 추가

--- SoleApp/SoleApp/Main/Home/CourseDetail/CourseDetailView.swift
+++ SoleApp/SoleApp/Main/Home/CourseDetail/CourseDetailView.swift
@@ -13,6 +13,14 @@ final class CourseDetailView: UIViewController {
     private let mapView = NMFMapView()
     private let scrollView = UIScrollView()
     private let contentView = UIView()
+    private let profileSectionView = ProfileSectionView(frame: .zero)
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        return stackView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationUI(title: "")
@@ -35,12 +43,24 @@ final class CourseDetailView: UIViewController {
             $0.width.equalToSuperview()
         })
         
-        [mapView].forEach({ contentView.addSubview($0) })
+        contentView.addSubview(stackView)
+        
+        stackView.snp.makeConstraints({
+            $0.edges.equalTo(contentView)
+        })
+        
+        [mapView,
+         profileSectionView].forEach({ stackView.addArrangedSubview($0) })
         
         mapView.snp.makeConstraints({
-            $0.top.equalToSuperview()
-            $0.horizontalEdges.equalToSuperview()
+//            $0.top.equalToSuperview()
+//            $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(300.0)
+        })
+        
+        profileSectionView.snp.makeConstraints({
+//            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(72.0)
         })
 
     }
