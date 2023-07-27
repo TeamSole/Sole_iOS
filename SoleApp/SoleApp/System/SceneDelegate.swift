093d0791abdd14e23e751d75a1667c075e130610
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-07-27 20:23:35 +0900

    Fix: 주석 제거

--- SoleApp/SoleApp/System/SceneDelegate.swift
+++ SoleApp/SoleApp/System/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let mainViewModel = AppDelegate.shared.mainViewModel
-        window?.rootViewController = UIHostingController(rootView: IntroView().environmentObject(mainViewModel)) // MainTabbarViewController()//UINavigationController(rootViewController: LoginViewController())//
+        window?.rootViewController = UIHostingController(rootView: IntroView().environmentObject(mainViewModel))
         window?.backgroundColor = .white
         window?.overrideUserInterfaceStyle = .light
         window?.makeKeyAndVisible()
