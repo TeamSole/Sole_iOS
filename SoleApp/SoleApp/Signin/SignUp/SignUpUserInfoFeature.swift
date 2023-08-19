1ae4d55a2728337c8b47a8fe77f1d4df4a5234ae
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-08-19 23:04:22 +0900

    Feat: 회원가입 시 유저 정보 입력 화면 Feature 추가

---
+++ SoleApp/SoleApp/Signin/SignUp/SignUpUserInfoFeature.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpUserInfoFeature.swift
+//  SoleApp
+//
+//  Created by SUN on 2023/08/19.
+//
+
+import ComposableArchitecture
+
+struct SignUpUserInfoFeature: Reducer {
+    struct State: Equatable {
+        
+    }
+    
+    enum Action: Equatable {
+        
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}
