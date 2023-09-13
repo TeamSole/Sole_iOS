e017d3a8b438d34fd89a3d19ff87431ed0e5fa94
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-09-13 21:54:29 +0900

    Feat: 나의 기록 client 추가

---
+++ SoleApp/SoleApp/Network/History/HistoryClient.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryClient.swift
+//  SoleApp
+//
+//  Created by SUN on 2023/09/13.
+//
+
+import Dependencies
+
+struct HistoryClient {
+    var getUserHistory: () async throws -> (UserProfileHistoryModelResponse)
+}
+
+extension HistoryClient: DependencyKey {
+    static var liveValue: HistoryClient  = HistoryClient(
+        getUserHistory: {
+            let request = API.makeDataRequest(HistoryTarget.getUserHistory)
+            let data = try await request.validate().serializingData().value
+            return try API.responseDecodeToJson(data: data, response: UserProfileHistoryModelResponse.self)
+        })
+}
