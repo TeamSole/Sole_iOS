e1120467bade36751a8217cec62662d38964e44c
Jang HuiSeon <xkzl8521@gmail.com>
2024-01-21 21:23:30 +0900

    Feat: 대전지역 데이터 location protocol 채택 및 준수

--- SoleApp/SoleApp/Data/Tasty/DaejeonLocation.swift
+++ SoleApp/SoleApp/Data/Tasty/DaejeonLocation.swift
@@ -7,17 +7,37 @@
 
 import Foundation
 
-enum DaejeonLocation: String {
+enum DaejeonLocation: String, LocationProtocol {
     case Whole = "DJ00"
     case Daedeok = "DJ01"
     case Dong = "DJ02"
     case Seo = "DJ03"
     case Yuseong = "DJ04"
     case Jung = "DJ05"
+    
+    var locationCode: String {
+        return self.rawValue
+    }
+    
+    var allCode: [String] {
+        return BusanLocation.allCases.map({ $0.rawValue }).filter({ $0 != "DJ00" })
+    }
+    
+    var mainLocationName: String {
+        return "대전"
+    }
+    
+    var isWholeLocation: Bool {
+        return self == .Whole
+    }
+    
+    var prefixCode: String {
+        return "DJ"
+    }
 
     var koreanName: String {
         switch self {
-        case .Whole: return "대전 전체"
+        case .Whole: return "전체"
         case .Daedeok: return "대덕구"
         case .Dong: return "동구"
         case .Seo: return "서구"
