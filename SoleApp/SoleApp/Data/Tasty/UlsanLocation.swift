54a0c15a26b3b1f67f93edaf2adae9885cb76b70
Jang HuiSeon <xkzl8521@gmail.com>
2024-01-21 22:06:01 +0900

    Feat: 울산지역 데이터 location protocol 채택 및 준수

--- SoleApp/SoleApp/Data/Tasty/UlsanLocation.swift
+++ SoleApp/SoleApp/Data/Tasty/UlsanLocation.swift
@@ -7,17 +7,37 @@
 
 import Foundation
 
-enum UlsanLocation: String {
+enum UlsanLocation: String, LocationProtocol {
     case Whole = "U00"
     case Nam = "U01"
     case Dong = "U02"
     case Buk = "U03"
     case Ulju = "U04"
     case Jung = "U05"
+    
+    var locationCode: String {
+        return self.rawValue
+    }
+    
+    var allCode: [String] {
+        return BusanLocation.allCases.map({ $0.rawValue }).filter({ $0 != "U00" })
+    }
+    
+    var mainLocationName: String {
+        return "울산"
+    }
+    
+    var isWholeLocation: Bool {
+        return self == .Whole
+    }
+    
+    var prefixCode: String {
+        return "U"
+    }
 
     var koreanName: String {
         switch self {
-        case .Whole: return "울산 전체"
+        case .Whole: return "전체"
         case .Nam: return "남구"
         case .Dong: return "동구"
         case .Buk: return "북구"
